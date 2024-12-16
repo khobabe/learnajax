d6e7a43b524c89c7b915459d94f81038dc8ff586
khobabe <khobabe007@gmail.com>
2024-12-17 01:53:46 +0530

    first ajax , using jquery , done and resolved some mistakes i made in the task model

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function store(Request $request){
+        $task = new Task;
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->save();
+
+        return response()->json([
+            "success" => true, 
+            "msg" => "data inserted successfully"
+        ]);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,15 +7,5 @@ use Illuminate\Http\Request;
 
 class Task extends Model
 {
-    public function store(Request $request){
-        $task = new Task;
-        $task->name = $request->name;
-        $task->description = $request->description;
-        $task->save();
-
-        return response()->json([
-            "success" => true, 
-            "msg" => "data inserted successfully"
-        ]);
-    }
+   
 }
