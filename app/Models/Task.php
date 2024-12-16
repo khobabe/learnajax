f92f0a1dc1c7279531b291f5019ae828d06228f7
khobabe <khobabe007@gmail.com>
2024-12-17 00:37:41 +0530

    controller method created to save the data into the database . also giving response in json

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Task extends Model
 {
-    //
+    public function store(Request $request){
+        $task = new Task;
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->save();
+
+        return response()->json([
+            "success" => true, 
+            "msg" => "data inserted successfully"
+        ]);
+    }
 }
